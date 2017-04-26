f63ccf77f26d9a30baf370f79b2c481f817551c4
Keith Stedman <keithstedman@gmail.com>
2017-04-26 15:48:31 -0700

    adding files

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rentals');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rentals'),
+      reviews: this.store.findAll('review')
+    });
   },
 
   actions: {
@@ -10,22 +13,6 @@ export default Ember.Route.extend({
       var newRental = this.store.createRecord('rentals', params);
       newRental.save();
       this.transitionTo('index');
-    },
-
-    update(rental, params) {
-      Object.keys(params).forEach(function(key) {
-       if(params[key]!==undefined) {
-         rental.set(key,params[key]);
-       }
-      });
-     rental.save();
-     this.transitionTo('index');
-   },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
     }
   }
-
 });
